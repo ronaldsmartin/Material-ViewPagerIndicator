2a018e4242b07195d11ee202afe7933690f74295
Ronald Martin <ronald@ronaldsmartin.com>
2016-10-01 17:17:32 -0700

    Make indicator a child view of ViewPager.
    This follows the pattern used by the official Support v4 widgets PagerTabStrip and PagerTitleStrip.

--- material-viewpagerindicator/src/main/java/com/itsronald/widget/ViewPagerIndicator.java
+++ material-viewpagerindicator/src/main/java/com/itsronald/widget/ViewPagerIndicator.java
@@ -2,15 +2,24 @@ package com.itsronald.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.ViewGroup;
 
 /**
  *
  */
-public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeListener {
+public class ViewPagerIndicator extends ViewGroup {
+
+    @Nullable
+    private ViewPager viewPager;
+
+    private int pageCount = 0;
 
 
     //region Constructors
@@ -34,23 +43,47 @@ public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeLi
 
     //endregion
 
-
-    //region ViewPager.OnPageChangeListener
-
     @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
     }
 
-    @Override
-    public void onPageSelected(int position) {
 
+    private class PageListener extends DataSetObserver
+            implements ViewPager.OnPageChangeListener, ViewPager.OnAdapterChangeListener {
+
+        //region ViewPager.OnPageChangeListener
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        //endregion
+
+        //region ViewPager.OnAdapterChangeListener
+
+        @Override
+        public void onAdapterChanged(@NonNull ViewPager viewPager, @Nullable PagerAdapter oldAdapter, @Nullable PagerAdapter newAdapter) {
+
+        }
+
+        //endregion
     }
 
-    @Override
-    public void onPageScrollStateChanged(int state) {
 
-    }
+    //region Accessors
+
 
     //endregion
 }
