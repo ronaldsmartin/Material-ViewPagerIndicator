ceaf808d6a5204e9f0d16b6334285f9733270aa9
Ronald Martin <ronald@ronaldsmartin.com>
2016-10-07 12:43:33 -0700

    Use ObjectAnimator for dot reveals when available.
    That is, instead of falling all the way back to the Animation API for all API levels between Lollipop and Honeycomb.

--- material-viewpagerindicator/src/main/java/com/itsronald/widget/IndicatorDotView.java
+++ material-viewpagerindicator/src/main/java/com/itsronald/widget/IndicatorDotView.java
@@ -1,6 +1,9 @@
 package com.itsronald.widget;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -14,6 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.Px;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.ScaleAnimation;
@@ -112,41 +116,87 @@ class IndicatorDotView extends ImageView {
     //endregion
 
     /**
-     * Start a Material reveal animation of this view.
+     * Start a Material reveal animation of this view with no delay.
      */
     void reveal() {
+        reveal(0);
+    }
+
+    /**
+     * Start a Material reveal animation of this view with a specified start delay.
+     *
+     * @param startDelay The start delay for the animation, in milliseconds.
+     */
+    void reveal(long startDelay) {
         final int centerX = getWidth() / 2;
         final int centerY = getHeight() / 2;
         final long animationDuration = getContext().getResources()
                 .getInteger(android.R.integer.config_shortAnimTime);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            startCircularReveal(centerX, centerY, animationDuration);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            startAnimatorReveal(centerX, centerY, animationDuration, startDelay);
         } else {
             startScaleReveal(centerX, centerY, animationDuration);
         }
     }
 
     /**
-     * Start a Material reveal animation of this view.
+     * Start a reveal animation of this view.
      *
      * @param centerX           The X point from which the animation starts.
      * @param centerY           The Y point from which the animation starts.
      * @param animationDuration How long the animation should last.
+     * @param startDelay        The start delay for the animation, in milliseconds.
      * @see #reveal()
      */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void startCircularReveal(int centerX, int centerY, long animationDuration) {
-        Animator revealAnimator = ViewAnimationUtils
-                .createCircularReveal(this, centerX, centerY, 0, dotRadius);
-        revealAnimator.setDuration(animationDuration);
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void startAnimatorReveal(int centerX, int centerY, long animationDuration, long startDelay) {
+        final Animator animator = revealAnimator(centerX, centerY);
+
+        animator.setDuration(animationDuration);
+        animator.setStartDelay(startDelay);
 
         setVisibility(VISIBLE);
-        revealAnimator.start();
+        animator.start();
     }
 
     /**
-     * Start a fallback reveal animation of this view for API versions < Lollipop.
+     * Create the API-level appropriate animator for this indicator's reveal.
+     *
+     * @param centerX The X point from which the animation starts.
+     * @param centerY The Y point from which the animation starts.
+     *
+     * @see #startAnimatorReveal(int, int, long, long)
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private Animator revealAnimator(int centerX, int centerY) {
+        final Animator animator;
+        /*
+         * The standard material circular reveal animation is exactly what we want.
+         *
+         * Since this view is already circular, a scale animation effectively simulates the
+         * material circular reveal on earlier API versions.
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            animator = ViewAnimationUtils
+                    .createCircularReveal(this, centerX, centerY, 0, dotRadius);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            final PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0, 1);
+            final PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0, 1);
+            animator = ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY);
+        } else {
+            final Animator scaleX = ObjectAnimator.ofFloat(this, "scaleX", 0, 1);
+            final Animator scaleY = ObjectAnimator.ofFloat(this, "scaleY", 0, 1);
+
+            final AnimatorSet animatorSet = new AnimatorSet();
+            animatorSet.playTogether(scaleX, scaleY);
+            animator = animatorSet;
+        }
+        return animator;
+    }
+
+    /**
+     * Start a fallback reveal animation of this view for API versions < Honeycomb.
      * <p>
      * It looks the same as the Material Circular Reveal animation (because this is already
      * a circle) but is fractionally slower to start.
