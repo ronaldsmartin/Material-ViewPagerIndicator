dc18d741c9e513b229c1a48a14fd614f90ad56c3
Ronald Martin <ronald@ronaldsmartin.com>
2016-10-13 14:15:00 -0700

    Add launcher activity to serve as app home menu.

---
+++ app/src/main/java/com/itsronald/materialviewpagerindicatorsample/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 Ronald Martin <hello@itsronald.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified 10/13/16 11:34 AM.
+ */
+
+package com.itsronald.materialviewpagerindicatorsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSimpleXmlExampleCardClick(View view) {
+        final Intent intent = new Intent(this, SimpleXmlExampleActivity.class);
+        startActivity(intent);
+    }
+}
