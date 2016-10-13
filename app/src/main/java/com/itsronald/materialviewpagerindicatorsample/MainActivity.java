530998f28ffca962b52c34f73b5a54c88301834c
Ronald Martin <ronald@ronaldsmartin.com>
2016-10-13 14:38:56 -0700

    Use main activity's toolbar as ActionBar.

--- app/src/main/java/com/itsronald/materialviewpagerindicatorsample/MainActivity.java
+++ app/src/main/java/com/itsronald/materialviewpagerindicatorsample/MainActivity.java
@@ -20,15 +20,19 @@ package com.itsronald.materialviewpagerindicatorsample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void onSimpleXmlExampleCardClick(View view) {
