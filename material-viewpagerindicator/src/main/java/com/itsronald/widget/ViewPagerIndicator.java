8ecbd3ba0ade862ff53f1303269c99bac99ed09a
xulingfeng <xlf2630@gmail.com>
2016-10-26 18:17:53 +0800

    bugfix
    https://github.com/ronaldsmartin/Material-ViewPagerIndicator/issues/14

--- material-viewpagerindicator/src/main/java/com/itsronald/widget/ViewPagerIndicator.java
+++ material-viewpagerindicator/src/main/java/com/itsronald/widget/ViewPagerIndicator.java
@@ -556,11 +556,11 @@ public class ViewPagerIndicator extends ViewGroup {
 
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            if (positionOffset > 0.5f) {
-                // Consider ourselves to be on the next page when we're 50% of the way there.
-                position++;
-            }
-            updateIndicatorPositions(position, positionOffset, false);
+//            if (positionOffset > 0.5f) {
+//                // Consider ourselves to be on the next page when we're 50% of the way there.
+//                position++;
+//            }
+//            updateIndicatorPositions(position, positionOffset, false);
         }
 
         @Override
@@ -574,6 +574,7 @@ public class ViewPagerIndicator extends ViewGroup {
             if (pageChangeAnimator != null) {
                 pageChangeAnimator.start();
             }
+            lastKnownCurrentPage = position;
         }
 
         @Override
